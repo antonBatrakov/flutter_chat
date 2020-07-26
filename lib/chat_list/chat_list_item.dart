c2a881866cbbe9246a72fe059e792671d413e287
Anton Batrakov <anton_batrakov@epam.com>
2020-07-26 16:14:33 +0300

    initial commit

---
+++ lib/chat_list/chat_list_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatListItem extends StatefulWidget {
+  @override
+  _ChatListItemState createState() => _ChatListItemState();
+}
+
+class _ChatListItemState extends State<ChatListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
